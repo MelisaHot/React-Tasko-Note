a86eb89fe85d490c6ca01ba125b68511655fa84b
Melisa Hot <melisa.hot.03@gmail.com>
2021-12-21 18:54:52 +0100

    Add new task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import {useState} from 'react'
 
 
@@ -27,18 +28,28 @@ function App() {
         }
     ]
 )
+//add task
 
+const addTask = (task) => {
+  console.log(task)
+}
 //delete task
 
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id ))
 }
 
+//Toggle reminder
+
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder}:task))
+}
   return (
     <div className="container">
       <Header title="Task Note"/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
-      <Tasks tasks={tasks} onDelete={deleteTask} />) 
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
       : ('No Tasks to show') }
     </div>
   );
