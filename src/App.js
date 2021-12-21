d69c87483ce0ec93cc17552c365c33daf2d4eb0e
Melisa Hot <melisa.hot.03@gmail.com>
2021-12-21 19:11:45 +0100

    Show Add Task

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {useState} from 'react'
 
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState (
     [
         {
@@ -31,7 +33,9 @@ function App() {
 //add task
 
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000)+1 
+  const newTask = {id, ...task }
+  setTasks([...tasks, newTask])
 }
 //delete task
 
@@ -46,8 +50,8 @@ const toggleReminder = (id) =>{
 }
   return (
     <div className="container">
-      <Header title="Task Note"/>
-      <AddTask onAdd={addTask} />
+      <Header onAdd= { () => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
       : ('No Tasks to show') }
