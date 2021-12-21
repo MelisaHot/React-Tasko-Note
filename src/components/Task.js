4bd73c6b28f8c6c798cdcc6ef78cc42ced70cf1e
Melisa Hot <melisa.hot.03@gmail.com>
2021-12-21 17:20:50 +0100

    Add new components Button and Task

---
+++ src/components/Task.js
@@ -0,0 +1,31 @@
+const task = [
+    {
+        id: 1, 
+        text: 'Doctrs Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2, 
+        text: 'Metting in School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3, 
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+    
+
+]
+function Task() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Task
